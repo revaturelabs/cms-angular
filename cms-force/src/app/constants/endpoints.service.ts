06652eefb1cf15bb5105b3f03f93f9caeef0a6da
zberson <zacharyberson@gmail.com>
2019-05-22 16:52:57 -0400

    fixed endpoint constants to start with backend URL

--- cms-force/src/app/constants/endpoints.service.ts
+++ cms-force/src/app/constants/endpoints.service.ts
@@ -6,15 +6,15 @@ import { Injectable } from '@angular/core';
 export class EndpointsService {
    public readonly baseURL = "http://localhost:8080";   // CHANGE TO HOST
 
-   public readonly CREATE_NEW_CONTENT: string = "/content";
-   public readonly GET_ALL_CONTENT: string = "/content";
-   public readonly GET_CONTENT_BY_ID: string = "/content/${id}";
-   public readonly UPDATE_CONTENT_BY_ID: string = "/content/${id}";
-   public readonly UPDATE_CONTENT_MODULES_BY_ID: string = "/content/${id}/modules";
-   public readonly DELETE_CONTENT_BY_ID: string = "/content/${id}";
-   public readonly GET_ALL_MODULES: string = "/module";
-   public readonly GET_MODULE_BY_ID: string = "/module/${id}";
-   public readonly GET_CONTENT_BY_TAG: string = "/module/${id}/content";
+   public readonly CREATE_NEW_CONTENT: string = this.baseURL + "/content";
+   public readonly GET_ALL_CONTENT: string = this.baseURL + "/content";
+   public readonly GET_CONTENT_BY_ID: string = this.baseURL + "/content/${id}";
+   public readonly UPDATE_CONTENT_BY_ID: string = this.baseURL + "/content/${id}";
+   public readonly UPDATE_CONTENT_MODULES_BY_ID: string = this.baseURL + "/content/${id}/modules";
+   public readonly DELETE_CONTENT_BY_ID: string = this.baseURL + "/content/${id}";
+   public readonly GET_ALL_MODULES: string = this.baseURL + "/module";
+   public readonly GET_MODULE_BY_ID: string = this.baseURL + "/module/${id}";
+   public readonly GET_CONTENT_BY_TAG: string = this.baseURL + "/module/${id}/content";
 
    constructor() { }
 }
