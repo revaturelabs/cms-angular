3b59a08c1450b4ea21f11a0f829b76426c7acd26
zberson <zacharyberson@gmail.com>
2019-05-22 16:06:39 -0400

    imported HttpClientModule, added fetcher services for module and content, added constants for endpoint URLs

--- cms-force/src/app/app.module.ts
+++ cms-force/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +9,18 @@ import { ContentFinderPageComponent } from './components/content-finder-page/con
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContentCreatorPageComponent,
-    ContentFinderPageComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ContentCreatorPageComponent,
+      ContentFinderPageComponent,
+      NavbarComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ cms-force/src/app/constants/endpoints.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class EndpointsService {
+   public readonly baseURL = "http://localhost:8080";   // CHANGE TO HOST
+
+   public readonly CREATE_NEW_CONTENT: string = "/content";
+   public readonly GET_ALL_CONTENTS: string = "/content";
+   public readonly GET_CONTENT_BY_ID: string = "/content/${id}";
+   public readonly UPDATE_CONTENT_BY_ID: string = "/content/${id}";
+   public readonly UPDATE_CONTENT_MODULES_BY_ID: string = "/content/${id}/modules";
+   public readonly DELETE_CONTENT_BY_ID: string = "/content/${id}";
+   public readonly GET_ALL_MODULES: string = "/module";
+   public readonly GET_MODULE_BY_ID: string = "/module/${id}";
+   public readonly GET_CONTENT_BY_TAG: string = "/module/${id}/content";
+
+   constructor() { }
+}
