f1dd7e2cb86c9b2ebeaeb2e8a6937a21876f31f8
Dustin <dustin@Dustins-MacBook-Pro.local>
2019-05-29 22:40:18 -0400

    little bits of testing

--- cms-force/e2e/src/app.e2e-spec.ts
+++ cms-force/e2e/src/app.e2e-spec.ts
@@ -2,15 +2,52 @@ import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  let createContent         : AppPage;
+  let title                 : string;
+  let url                   : string;
+  let selectedSubjects      : string[];
+  let description           : string;
 
-  beforeEach(() => {
-    page = new AppPage();
+  beforeAll(() => {
+    createContent = new AppPage();
+    title = Math.random().toString(36).substring(7);
+    url = 'www.testy.test';
+    selectedSubjects = ["elmo"];
+    description = "Test description."
+    createContent.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to cms-force!');
+  beforeEach(() => {
+  });
+
+  it('should accept title input', () => {
+    createContent.inputTitle(title);
+    expect(createContent.getTitleValue()).toEqual(title);
+  });
+
+  it('should accept url input', () => {
+    createContent.inputUrl(url);
+    expect(createContent.getUrlValue()).toEqual(url);
+  });
+
+  // it('should accept selected subjects input', () => {
+  //   createContent.inputSelectedSubjects(selectedSubjects);
+  //   expect(createContent.getTitleValue()).toEqual(title);
+  // });
+
+  it('should accept description input', () => {
+    createContent.inputDescription(description);
+    expect(createContent.getDescriptionValue()).toEqual(description);
+  });
+
+  it('should select Code radio button', () => {
+    createContent.clickDocumentRadio();
+    expect(createContent.getCheckedRadioValue()).toEqual('Document');
+  });
+
+  it('should select Document radio button', () => {
+      createContent.clickCodeRadio();
+      expect(createContent.getCheckedRadioValue()).toEqual('Code');
   });
 
   afterEach(async () => {
--- cms-force/e2e/src/app.po.ts
+++ cms-force/e2e/src/app.po.ts
@@ -1,11 +1,178 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
+    private title                : ElementFinder;
+    private url                  : ElementFinder;
+    private selectedSubjects     : ElementFinder;
+    private description          : ElementFinder;
+    private codeRadio            : ElementFinder;
+    private documentRadio        : ElementFinder;
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
-  }
+    constructor() {
+         this.title = this.getTitleInput();
+         this.url = this.getUrlInput();
+         this.selectedSubjects = this.getSelectedSubjectsInput();
+         this.description = this.getDescriptionInput();
+         this.codeRadio = this.getCodeRadio();
+         this.documentRadio = this.getDocumentRadio();
+    }
+
+    navigateTo() {
+      return browser.get(browser.baseUrl) as Promise<any>;
+    }
+
+    /**
+     * Returns the current URL in the browser
+     */
+    getCurrentURL() {
+        return browser.getCurrentUrl();
+    }
+
+    /**
+     * Returns the title element in the DOM
+     */
+    private getTitleInput() {
+        return element(by.css('[name="title"]'));
+    }
+
+    /**
+     * Sends the given input string to the title element in the DOM
+     * @param input
+     */
+    inputTitle(input: string) {
+        this.title.sendKeys(input);
+    }
+
+    /**
+     * Returns the current value of the title element in the DOM
+     */
+    getTitleValue() { 
+        return this.title.getAttribute('value');
+    }
+
+    /**
+     * Returns the url element in the DOM
+     */
+    private getUrlInput() {
+      return element(by.css('[name="url"]'));
+    }
+
+    /**
+     * Sends the given input string to the url element in the DOM
+     * @param input
+     */
+    inputUrl(input: string) {
+        this.url.sendKeys(input);
+    }
+
+    /**
+     * Returns the current value of the url element in the DOM
+     */
+    getUrlValue() { 
+        return this.url.getAttribute('value');
+    }
+
+    /**
+     * Returns the description element in the DOM
+     */
+    private getDescriptionInput() {
+      return element(by.css('[name="description"]'));
+    }
+
+    /**
+     * Sends the given input string to the description element in the DOM
+     * @param input
+     */
+    inputDescription(input: string) {
+        this.description.sendKeys(input);
+    }
+
+    /**
+     * Returns the current value of the description element in the DOM
+     */
+    getDescriptionValue() { 
+        return this.description.getAttribute('value');
+    }
+
+    /**
+     * Returns the url element in the DOM
+     */
+    private getSelectedSubjectsInput() {
+      return element(by.css('[name="subject"]'));
+    }
+
+    /**
+     * Sends the given input string to the url element in the DOM
+     * @param input
+     */
+    inputSelectedSubjects(input: string) {
+        this.selectedSubjects.sendKeys(input);
+    }
+
+    /**
+     * Returns the current value of the url element in the DOM
+     */
+    getSelectedSubjectsValue() { 
+        return this.selectedSubjects.getAttribute('value');
+    }
+
+    /**
+     * Returns the format radio button element in the DOM
+     */
+    private getCodeRadio(){
+        return element(by.css('[id="radio-2"]'));
+    }
+
+    /**
+     * Clicks the admin radio button element in the DOM
+     */
+    clickCodeRadio(){
+        this.codeRadio.click();
+    }
+
+    /**
+     * Returns the document radio button element in the DOM
+     */
+    private getDocumentRadio(){
+      return element(by.css('[id="radio-2"]'));
+    }
+
+    /**
+     * Clicks the document radio button element in the DOM
+     */
+    clickDocumentRadio(){
+        this.documentRadio.click();
+    }
+
+    /**
+     * Returns the checked radio element if one a radio element is checked
+     */
+    getCheckedRadioValue(){
+        if(element(by.css('[type="checked"]'))){
+            return element(by.css(':checked')).getAttribute('value');
+        }
+        else {
+            return '';
+        }
+    }
+
+    private getSubmitButton(){
+      // return element(by.xpath('/html/body/app-component/div/app-create-user/form/button'));
+      return element(by.css('[type="button"]'));
+    }
+
+    clickSubmitButton(){
+      this.getSubmitButton().click();
+    }
+
+    // getAlert(){
+    //   return element(by.css('body > app-component > div > app-create-user > form > fieldset:nth-child(2) > div'));
+    // }
+    // getUserNameAlert(){
+    //   return element(by.xpath('//*[@id="error-username"]'));
+    // }
+
+    refresh(){
+      return browser.refresh();
+    }
 }
