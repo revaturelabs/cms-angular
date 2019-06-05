9066a625c7ab8c965b9860a93195e5b1a04a84ae
zberson <zacharyberson@gmail.com>
2019-06-05 09:44:07 -0400

    removed unused endpoints and service methods

--- cms-force/src/app/services/content-fetcher.service.ts
+++ cms-force/src/app/services/content-fetcher.service.ts
@@ -35,6 +35,7 @@ export class ContentFetcherService {
       return this.http.put<HttpHeaderResponse>(this.endpoints.UPDATE_CONTENT_BY_ID.replace('${id}', id.toString()), body, { headers: this.HEADERS });
    }
 
+   /* Not yet implemented, Untested */
    updateContentModulesById(id: number, modules: Module[]): Observable<HttpHeaderResponse> {
       let body: string = JSON.stringify(modules);
       return this.http.put<HttpHeaderResponse>(this.endpoints.UPDATE_CONTENT_MODULES_BY_ID.replace('${id}', id.toString()), body, { headers: this.HEADERS });
@@ -44,28 +45,8 @@ export class ContentFetcherService {
       return this.http.delete<HttpHeaderResponse>(this.endpoints.DELETE_CONTENT_BY_ID.replace('${id}', id.toString()));
    }
 
-   getContentBySubjects(id: number, subjects: Module[]): Observable<Content[]> {
-      let body: string = JSON.stringify(subjects);
-      return this.http.post<Content[]>(this.endpoints.GET_CONTENT_BY_SUBJECTS.replace('${id}', id.toString()), body, { headers: this.HEADERS });
-   }
-
    filterContent(filter: Filter): Observable<Content[]> {
       let body: string = JSON.stringify(filter);
       return this.http.post<Content[]>(this.endpoints.FILTER_CONTENT, body, { headers: this.HEADERS });
    }
-
-   // filterContentByTitle(id: number, content: Content): Observable<Content[]> {
-   //    let body: string = JSON.stringify(content);
-   //    return this.http.post<Content[]>(this.endpoints.FILTER_CONTENT_BY_TITLE.replace('${id}', id.toString()), body, { headers: this.HEADERS });
-   // }
-
-   // filterContentByFormat(id: number, content: Content): Observable<Content[]> {
-   //    let body: string = JSON.stringify(content);
-   //    return this.http.post<Content[]>(this.endpoints.FILTER_CONTENT_BY_FORMAT.replace('${id}', id.toString()), body, { headers: this.HEADERS });
-   // }
-
-   // filterContentByModules(id: number, content: Content): Observable<Content[]> {
-   //    let body: string = JSON.stringify(content);
-   //    return this.http.post<Content[]>(this.endpoints.FILTER_CONTENT_BY_SUBJECTS.replace('${id}', id.toString()), body, { headers: this.HEADERS });
-   // }
 }
