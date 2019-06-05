083dd6faa8a89b95ab889ae441aa2b29c2996eac
John Park <luzeryzul@gmail.com>
2019-06-05 10:32:33 -0400

    Module index page for hierarchy view created

---
+++ cms-force/src/app/components/module-index-page/module-index-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-module-index-page',
+  templateUrl: './module-index-page.component.html',
+  styleUrls: ['./module-index-page.component.css']
+})
+export class ModuleIndexPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
