7ba212580333139ce96c98ec6ecc5d3a108b5a58
zberson <zacharyberson@gmail.com>
2019-06-05 13:52:27 -0400

    added scripts for protractor, organized protractor files into tests and population

---
+++ cms-force/e2e/src/populate/populateContent.po.ts
@@ -0,0 +1,179 @@
+import { browser, by, element, ElementFinder, protractor } from 'protractor';
+
+export class PopulateContentPage {
+   private title: ElementFinder;
+   private url: ElementFinder;
+   private selectedSubjects: ElementFinder;
+   private description: ElementFinder;
+   private codeRadio: ElementFinder;
+   private documentRadio: ElementFinder;
+
+   constructor() {
+      this.title = this.getTitleInput();
+      this.url = this.getUrlInput();
+      this.selectedSubjects = this.getSelectedSubjectsSelect();
+      this.description = this.getDescriptionInput();
+      this.codeRadio = this.getCodeRadio();
+      this.documentRadio = this.getDocumentRadio();
+   }
+
+   navigateTo() {
+      return browser.get(browser.baseUrl) as Promise<any>;
+   }
+
+   /**
+    * Returns the current URL in the browser
+    */
+   getCurrentURL() {
+      return browser.getCurrentUrl();
+   }
+
+   /**
+    * Returns the title element in the DOM
+    */
+   private getTitleInput() {
+      return element(by.css('[name="title"]'));
+   }
+
+   /**
+    * Sends the given input string to the title element in the DOM
+    * @param input: string
+    */
+   inputTitle(input: string) {
+      this.title.sendKeys(input);
+   }
+
+   /**
+    * Returns the current value of the title element in the DOM
+    */
+   getTitleValue() {
+      return this.title.getAttribute('value');
+   }
+
+   /**
+    * Returns the url element in the DOM
+    */
+   private getUrlInput() {
+      return element(by.css('[name="url"]'));
+   }
+
+   /**
+    * Sends the given input string to the url element in the DOM
+    * @param input
+    */
+   inputUrl(input: string) {
+      this.url.sendKeys(input);
+   }
+
+   /**
+    * Returns the current value of the url element in the DOM
+    */
+   getUrlValue() {
+      return this.url.getAttribute('value');
+   }
+
+   /**
+    * Returns the description element in the DOM
+    */
+   private getDescriptionInput() {
+      return element(by.css('[name="description"]'));
+   }
+
+   /**
+    * Sends the given input string to the description element in the DOM
+    * @param input: string
+    */
+   inputDescription(input: string) {
+      this.description.sendKeys(input);
+   }
+
+   /**
+    * Returns the current value of the description element in the DOM
+    */
+   getDescriptionValue() {
+      return this.description.getAttribute('value');
+   }
+
+   /**
+    * Returns the "select relevant subjects..." element in the DOM
+    */
+   private getSelectedSubjectsSelect() {
+      return element(by.css('[name="selectedSubjects"]'));
+   }
+
+   /**
+    * Enters the given input string[] into the selectedSubjects element in the DOM
+    * @param input: string[]
+    */
+   enterSelectedSubjects(inputs: string[]) {
+      this.selectedSubjects.click();
+      inputs.forEach((input) => {
+         browser.actions().sendKeys(input).perform();
+         browser.actions().sendKeys(protractor.Key.ENTER).perform();
+      });
+   }
+
+   /**
+    * Returns the code radio button element in the DOM
+    */
+   private getCodeRadio() {
+      return element(by.css('[id="Code"]'));
+   }
+
+   /**
+    * Clicks the code radio button element in the DOM
+    */
+   clickCodeRadio() {
+      browser.actions().mouseMove(this.codeRadio).click().perform();
+   }
+
+   /**
+    * Returns the document radio button element in the DOM
+    */
+   private getDocumentRadio() {
+      return element(by.id("Document"));
+   }
+
+   /**
+    * Clicks the document radio button element in the DOM
+    */
+   clickDocumentRadio() {
+      browser.actions().mouseMove(this.documentRadio).click().perform();
+   }
+
+   /**
+    * Returns the checked radio element if a radio element is checked
+    */
+   getCheckedRadioValue() {
+      if (element(by.css('[type="checked"]'))) {
+         return element(by.css(':checked')).getAttribute('id');
+      }
+      else {
+         return '';
+      }
+   }
+
+   private getSubmitButton() {
+      // return element(by.xpath('/html/body/app-component/div/app-create-user/form/button'));
+      return element(by.id("submitbutton"));
+   }
+
+   clickSubmitButton() {
+      this.getSubmitButton().click();
+   }
+
+   pressEnter() {
+      browser.actions().sendKeys(protractor.Key.ENTER).perform();
+   }
+
+   // getAlert(){
+   //   return element(by.css('body > app-component > div > app-create-user > form > fieldset:nth-child(2) > div'));
+   // }
+   // getUserNameAlert(){
+   //   return element(by.xpath('//*[@id="error-username"]'));
+   // }
+
+   refresh() {
+      return browser.refresh();
+   }
+}
---
+++ cms-force/e2e/src/test/app.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+
+describe('workspace-project App', () => {
+  let createContent         : AppPage;
+  let title                 : string;
+  let url                   : string;
+  let selectedSubjects      : string[];
+  let description           : string;
+
+  beforeAll(() => {
+    createContent = new AppPage();
+    title = Math.random().toString(36).substring(7);
+    url = 'http://www.test.com';
+    selectedSubjects = ["Java", "CSS"];
+    description = "Test description."
+    createContent.navigateTo();
+  });
+
+  beforeEach(() => {
+    browser.manage().timeouts().implicitlyWait(5000);
+  });
+
+  it('should accept title input', () => {
+    createContent.inputTitle(title);
+    expect(createContent.getTitleValue()).toEqual(title);
+  });
+
+  it('should accept url input', () => {
+    createContent.inputUrl(url);
+    expect(createContent.getUrlValue()).toEqual(url);
+  });
+
+  // only enters values
+  it('should accept selected subjects input', () => {
+    createContent.enterSelectedSubjects(selectedSubjects);
+    // browser.sleep(5000); // to manually check, uncomment.
+  });
+
+  it('should accept description input', () => {
+    createContent.inputDescription(description);
+    expect(createContent.getDescriptionValue()).toEqual(description);
+  });
+
+  it('should select Document radio button', () => {
+    createContent.clickDocumentRadio();
+    expect(createContent.getCheckedRadioValue()).toEqual('Document');
+  });
+
+  it('should select Code radio button', () => {
+    createContent.clickCodeRadio();
+    expect(createContent.getCheckedRadioValue()).toEqual('Code');
+  });
+
+  it('should click submit button', () => {
+    browser.sleep(2000);
+    createContent.clickSubmitButton();
+  });
+  
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
