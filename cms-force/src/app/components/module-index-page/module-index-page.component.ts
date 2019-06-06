a7374ed1887d2c3d3b3bae83c1c5c5d6c4297383
John Park <luzeryzul@gmail.com>
2019-06-06 10:26:46 -0400

    minor fix for module index page typescript

--- cms-force/src/app/components/module-index-page/module-index-page.component.ts
+++ cms-force/src/app/components/module-index-page/module-index-page.component.ts
@@ -16,7 +16,6 @@ export class ModuleIndexPageComponent implements OnInit {
   tablebool: boolean = false;
   contents: Content[] = [];
   moduleContents: Map<Module, Content[]> = new Map<Module, Content[]>();
-  colorbool: boolean = true;
 
   constructor(
     private cs: ContentFetcherService,
@@ -55,17 +54,6 @@ export class ModuleIndexPageComponent implements OnInit {
     /* sort contents by their id */
     this.moduleContents.set(module, response.sort(
       (a, b) => { return a.id - b.id }));
-  }
-
-  swapColor(): string {
-    if (this.colorbool){
-      this.colorbool = false;
-      return 'gray'
-    }
-    else{
-      this.colorbool = true;
-      return '#cccccc'
-    }
   }
 
 }
