cf088f22c9badc64328ac605e809475d5a6c5533
MartinD4574 <dcm9595@gmail.com>
2019-07-19 09:36:16 -0500

    Modified Prod Url

--- cms-force/src/environments/environment.prod.ts
+++ cms-force/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
    production: true,
-   cms_url: 'cms-dev-1861735057.us-east-1.elb.amazonaws.com'
+   cms_url: 'http://cms-dev-1861735057.us-east-1.elb.amazonaws.com:9009/CMSforce/as'
 };
