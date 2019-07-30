853859c7898c363c2398bdf5c5f9d10436f280df
Joshua Veit <joshuaveit1995@gmail.com>
2019-07-30 09:38:28 -0500

    modified the reports-page protractor tests to match the filter changes and re-added the time-graph dropdown selector

--- e2e/src/test/reports.e2e-spec.ts
+++ e2e/src/test/reports.e2e-spec.ts
@@ -7,6 +7,7 @@ describe('workspace-project App', () => {
     beforeAll(() => {
         reportsPage = new ReportsPage();
         reportsPage.navigateTo();
+        browser.sleep(15000);
     });
 
     it('should contain content in Code Examples', () => {
@@ -74,7 +75,7 @@ describe('workspace-project App', () => {
 
     it('should get filtered data', () => {
         reportsPage.clickFilterButton();
-        browser.sleep(5000);
+        browser.sleep(15000);
         expect(reportsPage.getChartData()).not.toBe(null);
     });
 
