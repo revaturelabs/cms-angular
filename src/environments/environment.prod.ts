4f51653870f735a764526d6a09869c259c847ca4
Quintin Donnelly <quinndonnelly22@gmail.com>
2019-08-01 05:07:30 -0400

    No port needed for the routing in cloud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 /** Environment variable holding cms url */
 export const environment = {
    production: true,
-   cms_url: 'http://cms-dev-1861735057.us-east-1.elb.amazonaws.com:9009/cmsforce'
+   cms_url: 'http://cms-dev-1861735057.us-east-1.elb.amazonaws.com/cmsforce'
 };
