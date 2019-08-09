f75fc8d41b12370285d887d418ea61f415af0cd8
Thomas Payton <thomaspayton53@gmail.com>
2019-08-09 15:32:38 -0500

    defualt selFormatFilter to All

--- src/app/components/reports-page/reports-page.component.ts
+++ src/app/components/reports-page/reports-page.component.ts
@@ -21,7 +21,7 @@ export class ReportsPageComponent implements OnInit {
 
   readonly formats: string[] = ["Code", "Document", "Powerpoint", "All"];
   selFormat: string = "All";
-  selFormatFilter = "";
+  selFormatFilter = "All";
   contents: Content[];
   moduleIDs: number[];
   selectedSubjects: string[] = [];
@@ -57,7 +57,7 @@ export class ReportsPageComponent implements OnInit {
 
     this.reportsService.reportsPage = this;
 
-    if(this.globalReports.metricsData) {
+    if(this.globalReports.metricsData ) {
 
       this.updateMetrics(this.globalReports.metricsData);
 
