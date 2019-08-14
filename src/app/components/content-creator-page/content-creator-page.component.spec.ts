fe797849ef262795f8bcb00bec7e47df117598f9
Nathan Medrano <ncamedrano1@tamu.edu>
2019-08-14 14:29:28 -0500

    Unit tests for Content Creator Page

--- src/app/components/content-creator-page/content-creator-page.component.spec.ts
+++ src/app/components/content-creator-page/content-creator-page.component.spec.ts
@@ -4,8 +4,10 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { MatProgressSpinnerModule} from '@angular/material';
+import { MatProgressSpinnerModule } from '@angular/material';
 import { ToastrModule } from 'ngx-toastr';
+
+
 
 import { Content } from 'src/app/models/Content';
 import { Link } from 'src/app/models/Link';
@@ -16,19 +18,22 @@ describe('ContentCreatorPageComponent', () => {
   let component: ContentCreatorPageComponent;
   let fixture: ComponentFixture<ContentCreatorPageComponent>;
 
+
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
-      declarations: [ ContentCreatorPageComponent ],
+      declarations: [ContentCreatorPageComponent],
       imports: [
         CommonModule,
         FormsModule,
-        NgSelectModule, 
+        NgSelectModule,
         MatProgressSpinnerModule,
         HttpClientTestingModule,
         ToastrModule.forRoot(),
       ]
     })
-    .compileComponents();
+      .compileComponents();
+
   }));
 
   beforeEach(() => {
@@ -37,7 +42,40 @@ describe('ContentCreatorPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  // First let's test general creation of the current component
+  it('Should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Next test that the desired input fields in the card exist
+  // i.e. Title, URL, Drop Down for Modules, and Description.
+  it('Should have a Title input box.', () => {
+    expect(document.getElementById('titleTextBox')).toBeTruthy();
+  })
+
+  it('Should have a URL input box.', () => {
+    expect(document.getElementById('urlTextBox')).toBeTruthy();
+  })
+
+  it('Should have a Select Relevant Modules drop down box.', () => {
+    expect(document.getElementById('subjectDropDown')).toBeTruthy();
+  })
+
+  it('Should have a Description box.', () => {
+    expect(document.getElementsByName('Description')).toBeTruthy();
+  })
+
+  it('Should have a Code radio button.', () => {
+    expect(document.getElementById('Code')).toBeTruthy();
+  })
+
+  it('Should have a Document radio button.', () => {
+    expect(document.getElementById('Document')).toBeTruthy();
+  })
+
+  it('Should have a Powerpoint radio button.', () => {
+    expect(document.getElementById('Powerpoint')).toBeTruthy();
+  })
+  // =====End Element creation finds=====
+
 });
