adae376d9b25b6460b4bd3c046d0724144f95ab9
MatthewOberlies <moberlies1337@gmail.com>
2019-08-15 09:56:13 -0500

    Added more names to html elements, and continued work on e2e tests

--- e2e/src/test/contentcreation.e2e-spec.ts
+++ e2e/src/test/contentcreation.e2e-spec.ts
@@ -61,7 +61,21 @@ describe('workspace-project App', () => {
     browser.sleep(1000);
     findContent.clickSearchButton();
     browser.waitForAngular();
-    
+    let rows = document.getElementsByTagName("tr");
+
+    // found is a 2d array of booleans to confirm the existence of the previously created content
+    // found will have 3 arrays which refer to title, url, and description in that order
+    // Each element in each of those 3 arrays will refer to the first, second, or third content that was created
+    let found: boolean[][] = [];
+
+    // Initially populating the boolean 2d array with falses
+    for(let i = 0; i < 3; i++) {
+      found.push([false, false, false]);
+    }
+
+    for(let i = rows.length - 1; i >= 0; i--) {
+      rows.item(i).children.namedItem("title");
+    }
   });
   
   afterEach(async () => {
