0509ea46171010674e80b61c3b3f73d8e6dda4af
Paul Hernandez <p_h74@txstate.edu>
2019-08-15 13:35:31 -0500

    Content Fetcher Service Unit Tests

--- src/app/services/content-fetcher.service.spec.ts
+++ src/app/services/content-fetcher.service.spec.ts
@@ -1,18 +1,60 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment'
 
 import { ContentFetcherService } from './content-fetcher.service';
 
 describe('ContentFetcherService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule,
-    ]
-  }));
+  let httpTestingController: HttpTestingController;
+  let service: ContentFetcherService;
+  let baseURL = environment.cms_url;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ContentFetcherService]
+  });
+  service = TestBed.get(ContentFetcherService);
+  httpTestingController = TestBed.get(HttpTestingController);
+});
+
+afterEach(() => {
+  httpTestingController.verify();
+});
 
   it('should be created', () => {
-    const service: ContentFetcherService = TestBed.get(ContentFetcherService);
     expect(service).toBeTruthy();
   });
+
+  it('getAllContent is working', fakeAsync(() => {
+    let response = {};
+
+    service.getAllContent().subscribe(
+      (receivedResponse: any) => {},
+      (error: any) => {}
+    );
+    const req = httpTestingController.expectOne(baseURL + '/content');
+    expect(req.request.method).toEqual("GET");
+    req.flush(response);
+    tick();
+
+  }));
+
+  
+  it('getContentByID is working', fakeAsync(() => {
+    let response = {};
+    
+
+    service.getContentByID(1).subscribe(
+      (receivedResponse: any) => {},
+      (error: any) => {}
+    );
+    const req = httpTestingController.expectOne(baseURL + '/content/1');
+    expect(req.request.method).toEqual("GET");
+    req.flush(response);
+    tick();
+
+  }));
+
+
 });
