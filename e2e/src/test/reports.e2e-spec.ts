62674f72b7fbd85e41c3a9d7e3767db12843b128
ArturoOrtizG559 <38798388+ArturoOrtizG559@users.noreply.github.com>
2019-08-20 16:55:35 -0700

    added working report page e2e test

--- e2e/src/test/reports.e2e-spec.ts
+++ e2e/src/test/reports.e2e-spec.ts
@@ -1,13 +1,179 @@
 import { browser, logging } from 'protractor';
 import { ReportsPage } from './reports.po';
+import { async } from '@angular/core/testing';
+import { ModuleCreatePage } from './modulecreation.po';
+import { AppPage } from './contentcreation.po';
+import { SearchPage } from './contentsearch.po';
+import { ModuleIndexPage } from './moduleindex.po';
 
 describe('workspace-project App', () => {
-    let reportsPage : ReportsPage;
+    let reportsPage           : ReportsPage;
+    let moduleCreate          : ModuleCreatePage;
+    let createContent         : AppPage;
+    let findContent           : SearchPage;
+    let moduleIndex           : ModuleIndexPage;
+    let title                 : string[] = [];
+    let url                   : string[] = [];
+    let selectedSubjects      : string[];
+    let description           : string[] = [];
+    
 
-    beforeAll(() => {
+    beforeAll(async () => {
         reportsPage = new ReportsPage();
+        moduleCreate = new ModuleCreatePage();
+        createContent = new AppPage();
+        findContent = new SearchPage();
+        moduleIndex = new ModuleIndexPage();
+
         reportsPage.navigateTo();
-        browser.sleep(3000);
+        reportsPage.clickFilterButton();
+        browser.sleep(1000);
+        let number = 0;
+        await reportsPage.getDifModules().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addOriginalValuesArrays(number);
+
+        await reportsPage.getAvgResources().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addOriginalValuesArrays(number);
+
+        await reportsPage.getCodeExamples().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addOriginalValuesArrays(number);
+
+        await reportsPage.getLectureNotes().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addOriginalValuesArrays(number);
+
+        await reportsPage.getPowerpoints().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addOriginalValuesArrays(number);
+
+       
+
+    });
+
+    it('should navigate to module creation', () =>{
+        moduleCreate.navigateTo();
+
+        this.selectedSubjects = Math.random().toString(36).substring(7);
+
+        moduleCreate.inputSubject(this.selectedSubjects);
+        expect(moduleCreate.getSubjectValue()).toEqual(this.selectedSubjects);
+        
+        moduleCreate.clickSubmitButton();
+        moduleCreate.acceptAlert();
+    });
+
+    it('should navigate back to reportsPage and click refresh button',async () => {
+        reportsPage.navigateTo();
+
+        reportsPage.clickFilterButton();
+
+        let number = 0;
+        await reportsPage.getDifModules().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getAvgResources().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getCodeExamples().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getLectureNotes().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getPowerpoints().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        expect(reportsPage.getNewValuesArray()[0]).toEqual(reportsPage.getOriginalValuesArray()[0] + 1);
+        reportsPage.resetNewValuesArray();
+    });
+
+    it('should navigate to content creation and add new content', async () =>{
+
+        await createContent.navigateTo();
+        //randomized the input for title, url (in format of: http://www.__random_string___.com ), and description for 3 times
+        for (let i = 0; i < 3; i++) {
+            title.push(Math.random().toString(36).substring(7));
+            url.push("http://www." + Math.random().toString(36).substring(7) + ".com");
+            description.push(Math.random().toString(36).substring(7));
+        }
+
+        for (let i = 0; i < 3; i++) {
+            //input 3 different titles for each creation with different types
+            createContent.inputTitle(title[i]);
+            expect(createContent.getTitleValue()).toEqual(title[i]);
+            //input 3 different urls for each creation with different types
+            createContent.inputUrl(url[i]);
+            expect(createContent.getUrlValue()).toEqual(url[i]);
+            //since we created two modules in the begining and we want to grab the one in the first index, which is index 0
+            createContent.enterSelectedSubjects([this.selectedSubjects[0]]);
+            //input 3 different desciptions for each creation with different types
+            createContent.inputDescription(description[i]);
+            expect(createContent.getDescriptionValue()).toEqual(description[i]);
+            //click on the corresponding Radio button with the index. 
+            //index 0 = Code ; index 1 = Document ; index 2 = PowerPoints
+            createContent.clickRadio(i);
+            browser.sleep(1000);
+            //click the submit button to submit the content. should show alert of "Successfully sent content"
+            createContent.clickSubmitButton();
+            createContent.acceptAlert();
+        }
+    });
+
+    it('should navigate back to reports page',async () =>{
+       await reportsPage.navigateTo();
+
+        reportsPage.clickFilterButton();
+
+        let number = 0;
+        await reportsPage.getDifModules().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getAvgResources().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getCodeExamples().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getLectureNotes().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        await reportsPage.getPowerpoints().getText().then(function (text){
+            number = +text;
+        });
+        reportsPage.addNewValuesArray(number);
+
+        //check code examples new size0
+        expect(reportsPage.getNewValuesArray()[2]).toEqual(reportsPage.getOriginalValuesArray()[2] + 1);
+        //check lecture notes new size
+        expect(reportsPage.getNewValuesArray()[3]).toEqual(reportsPage.getOriginalValuesArray()[3] + 1);
+        //check powerpoints new size
+        expect(reportsPage.getNewValuesArray()[4]).toEqual(reportsPage.getOriginalValuesArray()[4] + 1);
     });
 
     it('should contain content in Code Examples', () => {
--- e2e/src/test/reports.po.ts
+++ e2e/src/test/reports.po.ts
@@ -1,10 +1,39 @@
-import { browser, element, by, protractor } from 'protractor';
+import { browser, element, by, protractor, ElementFinder } from 'protractor';
 
 export class ReportsPage {
-    
+    private totalModules:ElementFinder;
+    private avgResource:ElementFinder;
+    private codeExample:ElementFinder;
+    private lectureNotes:ElementFinder;
+    private powerpoints:ElementFinder;
+    private selectedSubjectsSelect: ElementFinder;
+    private codeRadio: ElementFinder;
+    private allRadio: ElementFinder;
+    private documentRadio: ElementFinder;
+    private powerpointRadio: ElementFinder;
+
+    private originalValues: number[] = [];
+    private newValues: number[] = [];
+    private totalModulesNum: number;
+    private avgResourceNum: number;
+    private codeExampleNum:number;
+    private lectureNotesNum: number;
+    private powerpointsNum: number;
+
+    // private 
 
     constructor() {
-
+        this.totalModules = this.getDifModules();
+        this.avgResource = this.getAvgResources();
+        this.codeExample = this.getCodeExamples();
+        this.lectureNotes = this.getLectureNotes();
+        this.powerpoints = this.getPowerpoints();
+        this.selectedSubjectsSelect = this.getSelectedSubjectsSelect();
+        this.codeRadio = this.getCodeRadio();
+        this.allRadio = this.getAllRadio();
+        this.documentRadio = this.getDocumentRadio();
+        this.powerpointRadio = this.getPowerpointRadio();
+        
     }
 
     /**
@@ -14,12 +43,76 @@ export class ReportsPage {
         return browser.get(browser.baseUrl+"reports") as Promise<any>;
     }
 
+    resetNewValuesArray(){
+        this.newValues = [];
+    }
+
+    getNewValuesArray(){
+        return this.newValues;
+    }
+
+    addNewValuesArray(num:number){
+        this.newValues.push(num);
+    }
+
+    getOriginalValuesArray(){
+        return this.originalValues;
+    }
+
+    addOriginalValuesArrays(num:number){
+        this.originalValues.push(num);
+    }
+
+    setTotalModulesNum(num:number){
+        this.totalModulesNum = num;
+    }
+
+    getTotalModulesNum(){
+        return this.totalModulesNum;
+    }
+
+    setAvgResourceNum(num:number){
+        this.avgResourceNum = num;
+    }
+
+    getAvgResourceNum(num:number){
+        return this.avgResourceNum;
+    }
+
+    setCodeExampleNum(num:number){
+        this.codeExampleNum = num;
+    }
+
+    getCodeExampleNum(num:number){
+        return this.codeExampleNum;
+    }
+
+    setLectureNotesNum(num:number){
+        this.lectureNotesNum = num;
+    }
+
+    getLectureNotesNum(num:number){
+        return this.lectureNotesNum;
+    }
+
+    setPowerpointsNum(num:number){
+        this.powerpointsNum = num;
+    }
+
+    getPowerpointsNum(num:number){
+        return this.powerpointsNum;
+    }
+
     getCodeExamples() {
         return element(by.id("code_Ex"));
     }
 
     getLectureNotes() {
         return element(by.id("lect_Notes"));
+    }
+
+    getPowerpoints(){
+        return element(by.id("powerpoints"));
     }
 
     getDifModules() {
