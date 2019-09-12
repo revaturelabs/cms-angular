2e4765065902773dbb37da6281e87e1dbb605d23
Adam Hemmelgarn <adamhemmelgarn1995@yahoo.com>
2019-09-12 16:10:03 -0400

    Updated the urls used in the tests for the content fetcher, module fetcher, and module store service and changed the expected request method from post to get in content fetcher service

--- src/app/services/content-fetcher.service.spec.ts
+++ src/app/services/content-fetcher.service.spec.ts
@@ -163,8 +163,8 @@ describe('ContentFetcherService', () => {
       (receivedResponse: any) => { },
       (error: any) => { }
     );
-    const req = httpTestingController.expectOne(baseURL + '/search?title=${title}&format=${format}&modules=${modules}');
-    expect(req.request.method).toEqual("POST");
+    const req = httpTestingController.expectOne(baseURL + '/search?title=&format=&modules=');
+    expect(req.request.method).toEqual("GET");
     req.flush(response);
     tick();
 
