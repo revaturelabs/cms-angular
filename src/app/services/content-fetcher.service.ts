c44cad47848e8e454f597e07951cf3500b817edc
JonHess4 <38571343+JonHess4@users.noreply.github.com>
2019-09-12 13:24:46 -0400

    refactored the submit into a submit and sendSearch so the send search part could be used by the ngoOnInit method

--- src/app/services/content-fetcher.service.ts
+++ src/app/services/content-fetcher.service.ts
@@ -116,9 +116,13 @@ export class ContentFetcherService {
    @Cacheable()
    filterContent(filter: Filter): Observable<Content[]> {
       let modules: string = JSON.stringify(filter.modules);
-      modules = modules.replace('[','');
-      modules = modules.replace(']','');
-      console.log(modules);
+      if (!modules) {
+         modules = modules.replace('[','');
+         modules = modules.replace(']','');
+      } else {
+         modules = "";
+      }
+      console.log("Modules: " + modules);
       console.log("URL: " + this.endpoints.FILTER_CONTENT.replace('${title}',filter.title).replace('${format}', filter.format).replace('${modules}', modules))
       return this.http.get<Content[]>(this.endpoints.FILTER_CONTENT.replace('${title}',filter.title).replace('${format}', filter.format).replace('${modules}', modules), {withCredentials: true}).pipe(
          map( resp => resp as Content[])
