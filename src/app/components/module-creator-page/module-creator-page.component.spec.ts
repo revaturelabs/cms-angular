fd85f27cf34165e20c73d5786f99527471e5b188
Samuel Dorilas <samueldorilas@outlook.com>
2019-10-18 03:54:13 -0400

    Full coverage module-creator-page

--- src/app/components/module-creator-page/module-creator-page.component.spec.ts
+++ src/app/components/module-creator-page/module-creator-page.component.spec.ts
@@ -1,29 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule} from '@angular/material';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ToastrModule } from 'ngx-toastr';
-
 import { ModuleCreatorPageComponent } from './module-creator-page.component';
-// import { Component, DebugElement } from "@angular/core";
-import { By } from "@angular/platform-browser";
-import { ComponentRef } from '@angular/core';
 import { TreeModule} from 'angular-tree-component';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { MatCardModule } from '@angular/material/card';
+import { ModuleFetcherService } from 'src/app/services/module-fetcher.service';
+import { HttpHeaderResponse } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { Module } from 'src/app/models/Module';
 
 describe('ModuleCreatorPageComponent', () => {
   let component: ModuleCreatorPageComponent;
   let fixture: ComponentFixture<ModuleCreatorPageComponent>;
-  // let subjectEl: DebugElement
-  // let submitEl: DebugElement;
+  let moduleFetcherService:ModuleFetcherService;
+  let toastrService:ToastrService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ModuleCreatorPageComponent ],
+      providers: [ModuleFetcherService, ToastrService],
       imports: [
         FormsModule,
         MatProgressSpinnerModule,
@@ -38,48 +37,98 @@ describe('ModuleCreatorPageComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ModuleCreatorPageComponent);
+    moduleFetcherService = TestBed.get(ModuleFetcherService);
+    toastrService = TestBed.get(ToastrService);
     component = fixture.componentInstance;
-    // subjectEl = fixture.debugElement.query(By.css('input[name=subject]'));
-    // submitEl = fixture.debugElement.query(By.css('button'));
     fixture.detectChanges();
-
-    // Populating the module input field here
     component.subject = 'Java';
-
   });
 
-  // Testing creation of current component
   it('Should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // Testing to make sure the input field exists within the component
+  it('Should test ngDoCheck, update not called', () => {
+    component.nodes= ["node"];
+    let spy = spyOn(component.tree.treeModel, 'update');
+    component.ngDoCheck();
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('Should test validInput, false when cantBeNull is null', () => {
+    component.subject = null;
+    expect(component.validInput()).toBe(false);
+  });
+
   it('Should have a subject input field', () => {
     expect(document.getElementsByName('subject')).toBeTruthy();
   });
 
-  // Testing to make sure the submit button exists in the component
   it('Should have a Submit Modules button', () => {
     expect(document.getElementById('submitButton')).toBeTruthy();
   });
 
-  // Testing for module input
   it('Should give an error when no module input is written', () => {
     component.subject = '';
     expect(component.validInput()).toBeFalsy();
-    component.subject = 'Java';
   });
 
-  // Testing to make sure that we have good inputs
   it('Should return true if Module field is valid', () => {
     expect(component.validInput()).toBeTruthy();
   });
 
-  // Testing to make sure the resetVariables function works
-  it('Should reset the fields with the resetVariables function', () => {
+  it('Should reset the fields with the resetVariables function, subject', () => {
     component.resetVariables();
     expect(component.subject.length).toEqual(0);
-    expect(component.isSubmitting).toBeFalsy();
+  });
+
+  it('Should reset the fields with the resetVariables function, isSubmitting', () => {
+    component.resetVariables();
+    expect(component.subject.length).toEqual(0);
+  });
+
+  it('Should test submit success', () => {
+    let response :HttpHeaderResponse = new HttpHeaderResponse({headers: null, status: 200, statusText:"ok",url: "url"});
+    let  module:Module = new Module(1,"Java",null,null,null,null,null);
+    spyOn(moduleFetcherService,'createOrUpdateModule').and.returnValue(of(response));
+    spyOn(component.ms, "loadModules").and.returnValue(Promise.resolve([module]))
+    component.subject = "Java";
+    component.ms.subjectNameToModule.set("Java", module);
+    component.ms.subjectIdToModule.set(1,module);
+    component.tree.treeModel.activeNodeIds = {"1": true}
+    component.submit();
+    expect(toastrService.previousToastMessage).toBe('Successfully sent module.');
+  });
+
+  it('Should test submit, error 1', () => {
+    component.ms.subjectNameToModule = null;
+    spyOn(moduleFetcherService,'createOrUpdateModule').and.returnValue(of(null));
+    component.submit();
+    expect(toastrService.previousToastMessage).toBe('There was a problem creating a subject');
+  });
+
+  it('Should test submit, error 2', () => {
+    const observable: Observable<HttpHeaderResponse> = new Observable<HttpHeaderResponse>((observer) => {
+      observer.error({status: 400, statusText: "Bad Request"})
+      observer.complete();
+      return {unsubscribe() {console.log("submit test m-c-p unsubscribed")}};
+    });
+    spyOn(moduleFetcherService,'createOrUpdateModule').and.returnValue(observable);
+    component.submit();
+    expect(toastrService.previousToastMessage).toBe('Failed to create subject. Subject may already exist.');
+  });
+
+  it('Should test submit, falsy error', () => {
+    component.subject = null;
+    component.submit();
+    expect(toastrService.previousToastMessage).toBe('Please fill in the input field!');
+  });
+
+  it('Should test getModulesFromSubjects, return not empty', () => {
+    let  module:Module = new Module(1,"Java",null,null,null,null,null);
+    component.ms.subjectIdToModule.set(1, module);
+    let subject = {"1":true};
+    expect(component.getModulesFromSubjects(Object.entries(subject)).length).toBeGreaterThan(0);
   });
 
 });
